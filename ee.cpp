6ee5449b8497fdbb3f75a9aae366f167aa40e30d
Muhammad Daniyal <95414949+daniz786@users.noreply.github.com>
2021-12-02 16:16:25 +0500

    Add files via upload

---
+++ ee.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main ()
+{  
+
+int a,b,c;
+char opt;
+	cout<<"chose the operator "<<endl;
+	cin>>opt;
+	cout<<"Enter the first number"<<endl;
+	cin>>a;
+	cout<<"enter the second number"<<endl;
+	cin>>b;
+	if (opt='+')
+	{
+		
+	c=a+b;
+	cout<<"addition is "<<c<<endl;
+	}
+	else if ('-')
+	{
+		
+			c=a-b;
+	cout<<"subtraction is "<<c<<endl;
+	}
+	
+	
+	
+	
+}
